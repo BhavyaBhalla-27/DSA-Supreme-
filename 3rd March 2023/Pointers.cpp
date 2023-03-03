1d23949a249742fe240b2e2221a16c08b3742246
Bhavya Bhalla <114859167+BhavyaBhalla-27@users.noreply.github.com>
2023-03-03 22:25:55 +0530

    Class Code of 3rd March 2023

---
+++ 3rd March 2023/Pointers.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 5; // Declare and define the variables. Symbol table concept is used
+    cout<<a<<endl; // 5 will be displayed as output
+    cout<<&a<<endl; // Hexadecimal value will be in the output and this is the address of a. (Access address of a)
+    // Creation of pointer
+    int b = 6;
+    int *ptr = &b; // ptr is a pointer to b which contains integer data
+    cout<<*ptr<<endl; // This will print 6 as we have used dereference operator (Access value)
+    cout<<&ptr; // Address of ptr block
+    char ch = 'a';
+    char *ptr1 = &ch;
+    cout<<endl;
+    cout<<sizeof(ptr)<<endl; // size of pointer will be same for every data type
+    cout<<sizeof(ptr1)<<endl;
+    int *ptr2; // bad practice
+    // Rectifying the above mistake
+    int *ptr3 = nullptr;
+    int *ptr4 = 0;
+
+    // Copying the pointer to another pointer
+    int d = 5;
+    int *p = &d;
+    int *copy = p; // Copy the pointer to another pointer
+    cout<<*copy<<endl; // 5 will be output
+    cout<<*p<<endl; // 5 will be output
+    // pointer arithemetic 
+    p = p + 1;
+    cout<<p;
+    *p = *p + 1;
+    return 0;
+}
