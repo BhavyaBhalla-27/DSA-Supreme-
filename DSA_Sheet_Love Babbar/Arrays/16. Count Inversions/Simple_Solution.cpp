0b3bb9f9c9e37c16d31eb3985b6b9ff60c5d4786
Bhavya Bhalla <114859167+BhavyaBhalla-27@users.noreply.github.com>
2023-04-14 15:57:39 +0530

    Codes of Count Inversions

---
+++ DSA_Sheet_Love Babbar/Arrays/16. Count Inversions/Simple_Solution.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+long merge(vector<int>&arr,vector<int>&temp,long s,long mid,long e){
+        long i = s;
+        long j = mid + 1;
+        long k = s;
+        long invCount = 0;
+        while(i<=mid && j<=e){
+            if(arr[i] <= arr[j]){
+                temp[k++] = arr[i++];
+            }
+            else{ // Inversion count case
+                temp[k++] = arr[j++];
+                invCount += mid - i + 1;
+            }
+        }
+        while(i<=mid){
+            temp[k++] = arr[i++];
+        }
+        while(j<=e){
+            temp[k++] = arr[j++];
+        }
+        while(s<=e){
+            arr[s] = temp[s];
+            s++;
+        }
+        return invCount;
+    }
+    long mergeSort(vector<int>&arr,vector<int>&temp,long s,long e){
+        // Base condition
+        if(s>=e){
+            return 0;
+        }
+        long mid = s + (e-s)/2;
+        long invCount = 0;
+        // Recursive calls
+        invCount += mergeSort(arr,temp,s,mid);
+        invCount += mergeSort(arr,temp,mid+1,e);
+        invCount += merge(arr,temp,s,mid,e);
+        return invCount;
+        
+    }
+
+long countInversions(vector<int> arr) {
+    long invCount = 0;
+    vector<int>temp(arr.size(),0);
+    invCount = mergeSort(arr, temp, 0, arr.size() - 1);
+    return invCount;
+}
+int main(){
+    vector<int> arr{2,4,1,3,5};
+    int inversionCounts = countInversions(arr);
+    cout<<"The inversion counts for the input array is "<<inversionCounts;
+    return 0;
+}
